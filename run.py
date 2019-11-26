af17721460f7889a81a6ee467fcba51968a74f61
Endri Kacupaj <endrikacupaj@gmail.com>
2019-11-26 22:12:14 +0100

    - add models and dataset

---
+++ run.py
@@ -0,0 +1,132 @@
+"""Run baseline model"""
+import os
+import math
+import random
+import argparse
+from pathlib import Path
+import numpy as np
+
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchtext.data import BucketIterator
+
+from models.seq2seq import Seq2Seq
+from models.cnn import EncoderConv, DecoderConv
+from utils.checkpoint import Chechpoint
+from trainer.trainer import Trainer
+from utils.scorer import BleuScorer
+from evaluator.predictor import Predictor
+from utils.verbaldataset import VerbalDataset
+from utils.constants import (
+    SEED, CUDA, CPU, PAD_TOKEN, RNN_NAME, CNN_NAME,
+    ATTENTION_1, ATTENTION_2, QUESTION, QUERY
+)
+
+ROOT_PATH = Path(os.path.dirname(__file__))
+DEVICE = torch.device(CUDA if torch.cuda.is_available() else CPU)
+
+def parse_args():
+    """Add arguments to parser"""
+    parser = argparse.ArgumentParser(description='Verbalization dataset baseline models.')
+    parser.add_argument('--model', default=RNN_NAME, type=str,
+                        choices=[RNN_NAME, CNN_NAME], help='model to train the dataset')
+    parser.add_argument('--input', default=QUESTION, type=str,
+                        choices=[QUESTION, QUERY], help='use query as input')
+    parser.add_argument('--attention', default=ATTENTION_1, type=str,
+                        choices=[ATTENTION_1, ATTENTION_2], help='attention layer for rnn model')
+    parser.add_argument('--cover_entities', action='store_true', help='cover entities')
+    parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+    parser.add_argument('--epochs_num', default=50, type=int, help='number of epochs')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """Main method to run the models"""
+    args = parse_args()
+    dataset = VerbalDataset(ROOT_PATH)
+    query_as_input = True if args.input == QUERY else False
+    dataset.load_data_and_fields(cover_entities=args.cover_entities, query_as_input=query_as_input)
+    src_vocab, trg_vocab = dataset.get_vocabs()
+    train_data, valid_data, test_data = dataset.get_data()
+
+    print('--------------------------------')
+    print(f'Model: {args.model}')
+    print(f'Model input: {args.input}')
+    if args.model != CNN_NAME:
+        print(f'Attention: {args.attention}')
+    print(f'Cover entities: {args.cover_entities}')
+    print('--------------------------------')
+    print(f"Training data: {len(train_data.examples)}")
+    print(f"Evaluation data: {len(valid_data.examples)}")
+    print(f"Testing data: {len(test_data.examples)}")
+    print('--------------------------------')
+    print(f'Question example: {train_data.examples[0].src}')
+    print(f'Answer example: {train_data.examples[0].trg}')
+    print('--------------------------------')
+    print(f"Unique tokens in questions vocabulary: {len(src_vocab)}")
+    print(f"Unique tokens in answers vocabulary: {len(trg_vocab)}")
+    print('--------------------------------')
+    print(f'Batch: {args.batch_size}')
+    print(f'Epochs: {args.epochs_num}')
+    print('--------------------------------')
+
+    if args.model == RNN_NAME and args.attention == ATTENTION_1:
+        from models.rnn1 import EncoderRNN, DecoderRNN
+    elif args.model == RNN_NAME and args.attention == ATTENTION_2:
+        from models.rnn2 import EncoderRNN, DecoderRNN
+
+    # create model
+    encoder = EncoderRNN(src_vocab, DEVICE) if args.model == RNN_NAME else \
+              EncoderConv(src_vocab, DEVICE)
+    decoder = DecoderRNN(trg_vocab, DEVICE) if args.model == RNN_NAME else \
+              DecoderConv(trg_vocab, DEVICE)
+    model = Seq2Seq(encoder, decoder, args.model).to(DEVICE)
+
+    parameters_num = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'The model has {parameters_num:,} trainable parameters')
+    print('--------------------------------')
+
+    # create optimizer and criterion
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.CrossEntropyLoss(ignore_index=trg_vocab.stoi[PAD_TOKEN])
+
+    # train data
+    trainer = Trainer(optimizer, criterion, args.batch_size, DEVICE)
+    trainer.train(model, train_data, valid_data, num_of_epochs=args.epochs_num)
+
+    # load model
+    model = Chechpoint.load(model)
+
+    # generate test iterator
+    test_iterator = BucketIterator(test_data,
+                                   batch_size=args.batch_size,
+                                   sort_within_batch=True if args.model == RNN_NAME else False,
+                                   sort_key=lambda x: len(x.src),
+                                   device=DEVICE)
+
+    # evaluate model
+    test_loss = trainer.evaluator.evaluate(model, test_iterator)
+
+    # calculate blue score for test data
+    scorer = BleuScorer()
+    predictor = Predictor(model, src_vocab, trg_vocab, DEVICE)
+    scorer.data_score(test_data.examples, predictor)
+
+    print(f'| Test Loss: {test_loss:.3f} | Test PPL: {math.exp(test_loss):7.3f} |')
+    print(f'| Average BLEU score {scorer.average_score()} |')
+
+
+if __name__ == "__main__":
+    # set a seed value
+    SEED = 42
+    random.seed(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)
+    torch.backends.cudnn.enabled = False
+    torch.backends.cudnn.deterministic = True
+    main()
---
+++ utils/verbaldataset.py
@@ -0,0 +1,193 @@
+"""VerbalDataset"""
+import re
+import json
+from sklearn.model_selection import train_test_split
+from torchtext.data import Field, Example, Dataset
+
+from utils.constants import (
+    ANSWER_TOKEN, ENTITY_TOKEN, SOS_TOKEN, EOS_TOKEN,
+    SRC_NAME, TRG_NAME, TRAIN_PATH, TEST_PATH
+)
+class VerbalDataset(object):
+    """VerbalDataset class"""
+    TOKENIZE_SEQ = lambda self, x: x.replace("?", " ?").\
+                                     replace(".", " .").\
+                                     replace(",", " ,").\
+                                     replace("'", " '").\
+                                     split()
+    ANSWER_REGEX = r'\[.*?\]'
+    QUERY_DICT = {
+        'x': 'var_x',
+        'uri': 'var_uri',
+        '{': 'brack_open',
+        '}': 'brack_close',
+        '.': 'sep_dot',
+        'COUNT(uri)': 'count_uri'
+    }
+    def __init__(self, root_path):
+        self.train_path = str(root_path) + TRAIN_PATH
+        self.test_path = str(root_path) + TEST_PATH
+        self.train_data = None
+        self.valid_data = None
+        self.test_data = None
+        self.src_field = None
+        self.trg_field = None
+        self.earl_entities = self._read_earl_entites(str(root_path) + '/utils/earl_entities.json')
+
+    def _read_earl_entites(self, path):
+        entities = []
+        with open(path) as json_file:
+            entities = json.load(json_file)
+        keys = [ent['uid'] for ent in entities]
+        entities = dict(zip(keys, entities))
+        return entities
+
+    def _cover_answers(self, text):
+        """
+        Cover answers on text using an answer token
+        """
+        return re.sub(self.ANSWER_REGEX, ANSWER_TOKEN, text)
+
+    def _cover_entities(self, uid, question, answer):
+        """
+        Cover entities on a given text.
+        Since we use external entity recognizer it might
+        miss some entities or cover wrong ones.
+        A better approach will be to annotate all data
+        and cover all entities. This should improve model
+        performance.
+        """
+        # Try EARL for covering entities
+        # EARL results are serialized
+        data = self.earl_entities[uid]
+        question_entities = data['question_entities']
+        answer_entities = data['answer_entities']
+        # we cover all recognized entitries by the same token
+        # this has to be improved based on the number of entities.
+        # For example if we have 2 entities we create 2 tokens e.g. <ent1> <ent2>
+        # In this way we know the position of each entity in the translated output
+        for ent in question_entities: question = question.replace(ent, ENTITY_TOKEN)
+        for ent in answer_entities: answer = answer.replace(ent, ENTITY_TOKEN)
+
+        return question, answer
+
+    def _prepare_query(self, query, cover_entities):
+        """
+        trasnform query from this:
+        SELECT DISTINCT COUNT(?uri) WHERE { ?x <http://dbpedia.org/ontology/commander> <http://dbpedia.org/resource/Andrew_Jackson> . ?uri <http://dbpedia.org/ontology/knownFor> ?x  . }
+        To this:
+        select distinct count_uri where brack_open var_x commander Andrew Jackson sep_dot var_uri known for var_x sep_dot brack_close
+        """
+        query = query.replace('\n', ' ')\
+                     .replace('\t', '')\
+                     .replace('?', '')\
+                     .replace('{?', '{ ?')\
+                     .replace('>}', '> }')\
+                     .replace('uri}', 'uri }').strip()
+        query = query.split()
+        new_query = []
+        for q in query:
+            if q in self.QUERY_DICT:
+                q = self.QUERY_DICT[q]
+            if 'http' in q:
+                if 'dbpedia.org/ontology' in q or 'dbpedia.org/property' in q:
+                    q = q.rsplit('/', 1)[-1].lstrip('<').rstrip('>')
+                    q = filter(None, re.split("([A-Z][^A-Z]*)", q))
+                    q = ' '.join(q)
+                elif 'www.w3.org/1999/02/22-rdf-syntax-ns#type' in q:
+                    q = 'type'
+                elif cover_entities:
+                    q = ENTITY_TOKEN
+                else:
+                    q = q.rsplit('/', 1)[-1].lstrip('<').rstrip('>').replace('_', ' ')
+            new_query.append(q.lower())
+
+        assert new_query[-1] == 'brack_close', 'Query not ending with a bracket.'
+        return ' '.join(new_query)
+
+    def _extract_question_answer(self, train, test):
+        return [[data['question'], data['verbalized_answer']] for data in train], \
+                [[data['question'], data['verbalized_answer']] for data in test]
+
+    def _extract_query_answer(self, train, test):
+        return [[data['query'], data['verbalized_answer']] for data in train], \
+                [[data['query'], data['verbalized_answer']] for data in test]
+
+    def _make_torchtext_dataset(self, data, fields):
+        examples = [Example.fromlist(i, fields) for i in data]
+        return Dataset(examples, fields)
+
+    def load_data_and_fields(self, cover_entities=False, query_as_input=False):
+        """
+        Load verbalization data
+        Create source and target fields
+        """
+        train, test, val = [], [], []
+        # read data
+        with open(self.train_path) as json_file:
+            train = json.load(json_file)
+
+        with open(self.test_path) as json_file:
+            test = json.load(json_file)
+
+        # cover answers
+        for data in train: data.update((k, self._cover_answers(v)) for k, v in data.items() if k == "verbalized_answer")
+        for data in test: data.update((k, self._cover_answers(v)) for k, v in data.items() if k == "verbalized_answer")
+
+        # cover entities or prepare query if query is used
+        if cover_entities or query_as_input:
+            for data in [train, test]:
+                for example in data:
+                    uid = example['uid']
+                    question = example['question']
+                    answer = example['verbalized_answer']
+                    query = example['query']
+                    if query_as_input: query = self._prepare_query(query, cover_entities)
+                    if cover_entities: question, answer = self._cover_entities(uid, question, answer)
+                    example.update(question=question, verbalized_answer=answer, query=query)
+
+
+        # extract question-answer or query-answer pairs
+        if query_as_input:
+            train, test = self._extract_query_answer(train, test)
+        else:
+            train, test = self._extract_question_answer(train, test)
+
+        # split test data to val-test
+        test, val = train_test_split(test, test_size=0.5, shuffle=False)
+
+        # create fields
+        self.src_field = Field(tokenize=self.TOKENIZE_SEQ,
+                               init_token=SOS_TOKEN,
+                               eos_token=EOS_TOKEN,
+                               lower=True,
+                               include_lengths=True,
+                               batch_first=True)
+        self.trg_field = Field(tokenize=self.TOKENIZE_SEQ,
+                               init_token=SOS_TOKEN,
+                               eos_token=EOS_TOKEN,
+                               lower=True,
+                               batch_first=True)
+
+        fields_tuple = [(SRC_NAME, self.src_field), (TRG_NAME, self.trg_field)]
+
+        # create toechtext datasets
+        self.train_data = self._make_torchtext_dataset(train, fields_tuple)
+        self.valid_data = self._make_torchtext_dataset(val, fields_tuple)
+        self.test_data = self._make_torchtext_dataset(test, fields_tuple)
+
+        # build vocabularies
+        self.src_field.build_vocab(self.train_data, min_freq=2)
+        self.trg_field.build_vocab(self.train_data, min_freq=2)
+
+    def get_data(self):
+        """Return train, validation and test data objects"""
+        return self.train_data, self.valid_data, self.test_data
+
+    def get_fields(self):
+        """Return source and target field objects"""
+        return self.src_field, self.trg_field
+
+    def get_vocabs(self):
+        """Return source and target vocabularies"""
+        return self.src_field.vocab, self.trg_field.vocab
